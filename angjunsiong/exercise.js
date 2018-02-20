e28fe806380d7bb66d28bd3944a9a8de47bcf98b
angjunsiong <angjunsiong@gmail.com>
2018-02-20 12:17:42 -0500

    Updated exercise file(s)

--- angjunsiong/exercise.js
+++ angjunsiong/exercise.js
@@ -10,6 +10,8 @@ exercise.load = function(salary, boston){
     // salary is the upper limit that the user keys inside the box
 };
 
+console.log(exercise.salaries)
+
 // get salaries larger than given salary
 exercise.findBiggerSalaries = function(){
 
@@ -22,11 +24,23 @@ exercise.findBiggerSalaries = function(){
     //     Example: salaries larger than 300K is 8
     // ---------------------------------------------------
 
-    if (boston.data.data)
+    var hello = exercise.largerSalary();
+    var SalaryList = [];
+
+    var dataLength = exercise.salaries.length;
+    
+    for (i=0; i < dataLength; i++) {    
+            if (hello[i] === "True"){
+            SalaryList.push(exercise.salaries[i][18]);
+            }
+    }
+
+return SalaryList;
+
 };
 
 // filter function
-exercise.largerSalary = function largerSalary(item){
+exercise.largerSalary = function largerSalary(){
 
     // ---------------------------------------------------
     //   YOUR CODE
@@ -35,4 +49,19 @@ exercise.largerSalary = function largerSalary(item){
     //     is larger than exercise.salary
     // ---------------------------------------------------
 
-};
+    var BooleanIndicator = [];
+    dataLength = exercise.salaries.length;
+
+    for (i=0; i < dataLength; i++) {    
+        currentSalary = Number(exercise.salaries[i][18])
+    
+        if (currentSalary > exercise.salary){
+            BooleanIndicator.push("True");
+            }
+        else{
+        BooleanIndicator.push("False");
+            }
+        console.log(BooleanIndicator);
+    }
+    return BooleanIndicator;
+}
